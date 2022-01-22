3b860b3a49399d545f61c90e2028105e6c59c37d
morgan5556 <72217433+morgan5556@users.noreply.github.com>
2022-01-22 23:32:16 +0000

    added main class structure
    Added the class structure for the game, as well as the basic part of the Player class.

---
+++ src/main.py
@@ -0,0 +1,31 @@
+class NoughtsAndCrosses:
+    def __init__(self):
+        self.board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+
+    def __run_program_loop(self):
+        pass
+
+    def __display_menu(self):
+        pass
+
+    def __display_board(self):
+        pass
+
+    def __add_piece(self):
+        pass
+
+    def __check_for_win(self):
+        pass
+
+    def __add_player(self):
+        pass
+
+class Player:
+    def __init__(self):
+        self.name = ""
+        self.score = ""
+        self.symbol = ""
+
+    
+
+    
